1416590b939f0c74313491cb1260aa716d97b3a2
elizabethceline <elizabethceline04@gmail.com>
2024-06-09 20:34:32 +0700

    fix: btn hover, box modul

--- resources/views/user/module.blade.php
+++ resources/views/user/module.blade.php
@@ -33,27 +33,27 @@
         <div class="w-full flex items-center flex-col">
             <div class="title-container w-9/12 py-10">
                 <h1
-                    class="text-5xl font-extrabold drop-shadow-2xl text-[var(--contrast)] uppercase border-b-4 border-[var(--contrast)]">
+                    class="text-3xl sm:text-5xl font-extrabold drop-shadow-2xl text-[var(--contrast)] uppercase border-b-4 border-[var(--contrast)]">
                     Modul Pembelajaran</h1>
             </div>
-            <div class="grid grid-rows-2 grid-cols-2 w-9/12 gap-10">
+            <div class="sm:grid grid-rows-2 grid-cols-2 w-9/12 gap-10">
                 @foreach ($modules as $module)
                     <div
-                        class="module-container w-full bg-[var(--primary)] h-[250px] flex flex-col justify-center rounded-xl shadow-xl">
-                        <div class="flex items-center">
-                            <div class="pl-7">
+                        class="module-container w-full bg-[var(--primary)] flex flex-col justify-center rounded-xl shadow-xl mt-4 sm:mt-0 py-4 sm:py-1">
+                        <div class="flex items-center flex-col sm:flex-row">
+                            <div class="sm:pl-7">
                                 <img src="{{ asset($module['image']) }}" class="w-[120px]">
                             </div>
                             <div class="h-full flex flex-col justify-center px-7">
                                 <h2 class="text-xl font-bold py-2">{{ $module['title'] }}</h2>
-                                <p class="text-justify max-w-[400px]">{{ $module['description'] }}</p>
+                                <p class="max-w-[400px]">{{ $module['description'] }}</p>
                             </div>
                         </div>
                         <div class="w-full flex px-7 py-4">
-                            <a class="w-1/2 mr-3 rounded-lg bg-[var(--neutral)] h-[40px] font-semibold flex justify-center 
+                            <a class="w-1/2 mr-3 rounded-lg bg-[var(--neutral)] hover:bg-[var(--secondary)] transition-all duration-300 ease-in-out h-[40px] font-semibold flex justify-center
                         items-center"
                                 href="{{ $module['module'] }}">Modul</a>
-                            <a class="w-1/2 rounded-lg bg-[var(--neutral)] h-[40px] font-semibold flex justify-center 
+                            <a class="w-1/2 rounded-lg bg-[var(--neutral)] hover:bg-[var(--secondary)] transition-all duration-300 ease-in-out h-[40px] font-semibold flex justify-center
                         items-center"
                                 href="{{ $module['assessment'] }}">Latihan Soal</a>
                         </div>
